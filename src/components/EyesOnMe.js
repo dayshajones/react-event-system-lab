a5327303bfbfabb78e3407e13e8445bd90772782
“Daysha <+>
2022-04-13 11:32:00 -0700

    commit tree 992edd8f44434f269b1bb849a03f6ca650b02248

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,28 @@
 // Code EyesOnMe Component Here
+   
+import React, {Component} from 'react'
+
+class EyesOnMe extends Component {
+
+    focusEyes = () => {
+        console.log('Good!')
+    }
+
+    blurEyes = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    render() {
+        return (
+            <div>
+                <button
+                onFocus={this.focusEyes}
+                onBlur={this.blurEyes}
+                >Buddy</button>
+            </div>
+        )
+    }
+
+}
+
+export default EyesOnMe
